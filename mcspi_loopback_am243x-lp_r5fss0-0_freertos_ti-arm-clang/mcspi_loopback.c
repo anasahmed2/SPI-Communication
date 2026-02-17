ff2fde3922ae6ed4506a78286788d9a63ec8ac92
Anas Ahmed <anas31ahmed03@gmail.com>
2026-02-17 15:06:38 -0800

    simple

--- mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
+++ mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
@@ -1,5 +1,5 @@
 /*
- * spi_slave_task.c (Simple Echo Loop)
+ * spi_slave_task.c (Raw Print)
  */
 #include <string.h>
 #include <kernel/dpl/DebugP.h>
@@ -9,7 +9,8 @@
 #include "ti_drivers_open_close.h"
 #include "ti_board_open_close.h"
 
-#define APP_MCSPI_MSGSIZE   (512U)
+#define APP_MCSPI_MSGSIZE   (64U)
+#define NUM_PACKETS         (10U)
 #define MCSPI_CHANNEL_NUM   (0U) 
 
 uint8_t gSlaveTxBuffer[APP_MCSPI_MSGSIZE] __attribute__((aligned(128)));
@@ -18,29 +19,37 @@ uint8_t gSlaveRxBuffer[APP_MCSPI_MSGSIZE] __attribute__((aligned(128)));
 void spi_main_task(void *args)
 {
     MCSPI_Transaction spiTransaction;
+    uint32_t pkt;
+
     Drivers_open();
     Board_driversOpen();
     
-    DebugP_log("[SLAVE] Ready. Echo Loop Running.\r\n");
+    DebugP_log("[SLAVE] Ready (Printing Raw Data).\r\n");
 
-    /* Initial TX: 0xAA */
-    memset(gSlaveTxBuffer, 0xAA, APP_MCSPI_MSGSIZE);
+    for(pkt = 0; pkt < NUM_PACKETS; pkt++)
+    {
+        /* Clear Buffer with 0xCC (Dummy) */
+        memset(gSlaveRxBuffer, 0xCC, APP_MCSPI_MSGSIZE);
 
-    while(1) {
         MCSPI_Transaction_init(&spiTransaction);
         spiTransaction.channel = MCSPI_CHANNEL_NUM;
         spiTransaction.count   = APP_MCSPI_MSGSIZE;
         spiTransaction.txBuf   = (void *)gSlaveTxBuffer;
         spiTransaction.rxBuf   = (void *)gSlaveRxBuffer;
         
-        /* Block until transfer complete */
         if(MCSPI_transfer(gMcspiHandle[CONFIG_MCSPI0], &spiTransaction) == SystemP_SUCCESS) {
-            /* Copy RX to TX for NEXT transfer */
-            memcpy(gSlaveTxBuffer, gSlaveRxBuffer, APP_MCSPI_MSGSIZE);
+            /* Print first 4 bytes */
+            DebugP_log("[SLAVE] Pkt %d: %02X %02X %02X %02X\r\n", 
+                       pkt, gSlaveRxBuffer[0], gSlaveRxBuffer[1], gSlaveRxBuffer[2], gSlaveRxBuffer[3]);
         } else {
-            /* Reset on error */
+            DebugP_log("[SLAVE] Error.\r\n");
             MCSPI_close(gMcspiHandle[CONFIG_MCSPI0]);
             MCSPI_open(CONFIG_MCSPI0, NULL);
         }
     }
+    
+    DebugP_log("[SLAVE] Done.\r\n");
+    Board_driversClose();
+    Drivers_close();
+    vTaskDelete(NULL);
 }
