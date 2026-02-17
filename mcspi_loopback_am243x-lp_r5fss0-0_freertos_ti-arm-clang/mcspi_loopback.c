5c7a1869ddad0853ef20979d8ceb4d0c343b7f85
Anas Ahmed <anas31ahmed03@gmail.com>
2026-02-16 20:31:16 -0800

    idk

--- mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
+++ mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
@@ -1,5 +1,5 @@
 /*
- * spi_slave_task.c (Full Duplex Fix)
+ * spi_slave_task.c (Debug 0xAA)
  */
 #include <string.h>
 #include <kernel/dpl/DebugP.h>
@@ -11,7 +11,7 @@
 #include "ti_board_open_close.h"
 
 #define APP_MCSPI_MSGSIZE   (512U)
-#define NUM_PACKETS         (10U) /* 10 Packets */
+#define NUM_PACKETS         (10U)
 #define MCSPI_CHANNEL_NUM   (0U) 
 
 uint8_t gSlaveTxBuffer[APP_MCSPI_MSGSIZE] __attribute__((aligned(128)));
@@ -25,35 +25,34 @@ void spi_main_task(void *args)
 
     Drivers_open();
     Board_driversOpen();
-    DebugP_log("[SLAVE] Ready (Full Duplex).\r\n");
+    DebugP_log("[SLAVE] Ready (Sending 0xAA).\r\n");
 
-    /* Initial Setup: Send 0xAA (Garbage) for first packet */
+    /* Pre-fill TX buffer with 0xAA once */
     memset(gSlaveTxBuffer, 0xAA, APP_MCSPI_MSGSIZE);
 
     for(pkt = 0; pkt < NUM_PACKETS; pkt++)
     {
-        /* Clear RX to clean state */
-        memset(gSlaveRxBuffer, 0xCC, APP_MCSPI_MSGSIZE);
+        /* Clear RX to verify we are receiving master data */
+        memset(gSlaveRxBuffer, 0x00, APP_MCSPI_MSGSIZE);
 
         MCSPI_Transaction_init(&spiTransaction);
         spiTransaction.channel = MCSPI_CHANNEL_NUM;
         spiTransaction.count   = APP_MCSPI_MSGSIZE;
-        spiTransaction.txBuf   = (void *)gSlaveTxBuffer; // Pre-loaded with prev packet data
-        spiTransaction.rxBuf   = (void *)gSlaveRxBuffer; // Receiving new data
+        spiTransaction.txBuf   = (void *)gSlaveTxBuffer; // Always 0xAA
+        spiTransaction.rxBuf   = (void *)gSlaveRxBuffer;
         spiTransaction.args    = NULL;
 
-        /* Block until Master Transaction */
+        /* Wait for transfer */
         status = MCSPI_transfer(gMcspiHandle[CONFIG_MCSPI0], &spiTransaction);
         
         if(status == SystemP_SUCCESS) {
-             /* CRITICAL: Copy received data to TX buffer for NEXT transaction */
-             memcpy(gSlaveTxBuffer, gSlaveRxBuffer, APP_MCSPI_MSGSIZE);
-             DebugP_log("[SLAVE] Processed Pkt %d\r\n", pkt);
+             /* Check first byte just for log */
+             DebugP_log("[SLAVE] Rx Pkt %d. First Byte: %d\r\n", pkt, gSlaveRxBuffer[0]);
         } else {
-             DebugP_log("[SLAVE] Error %d at pkt %d. Resync...\r\n", status, pkt);
+             DebugP_log("[SLAVE] Error %d at pkt %d.\r\n", status, pkt);
+             /* Resync attempt */
              MCSPI_close(gMcspiHandle[CONFIG_MCSPI0]);
              MCSPI_open(CONFIG_MCSPI0, NULL);
-             continue;
         }
     }
 
