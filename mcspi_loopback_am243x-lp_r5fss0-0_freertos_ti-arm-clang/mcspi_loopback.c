972fc1d8635bc295a5557c50d3d6ccab358b18e7
Anas Ahmed <anas31ahmed03@gmail.com>
2026-02-17 16:14:52 -0800

    bi-directional data transfer

--- mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
+++ mcspi_loopback_am243x-lp_r5fss0-0_freertos_ti-arm-clang/mcspi_loopback.c
@@ -1,5 +1,5 @@
 /*
- * spi_slave_task.c (Raw Print)
+ * spi_slave_task.c (Receive All, Send All)
  */
 #include <string.h>
 #include <kernel/dpl/DebugP.h>
@@ -16,6 +16,9 @@
 uint8_t gSlaveTxBuffer[APP_MCSPI_MSGSIZE] __attribute__((aligned(128)));
 uint8_t gSlaveRxBuffer[APP_MCSPI_MSGSIZE] __attribute__((aligned(128)));
 
+/* Store received data to send back later */
+uint8_t gStorage[NUM_PACKETS][APP_MCSPI_MSGSIZE]; 
+
 void spi_main_task(void *args)
 {
     MCSPI_Transaction spiTransaction;
@@ -24,27 +27,40 @@ void spi_main_task(void *args)
     Drivers_open();
     Board_driversOpen();
     
-    DebugP_log("[SLAVE] Ready (Printing Raw Data).\r\n");
+    /* --- PHASE 1: RECEIVE ALL --- */
+    DebugP_log("[SLAVE] Ready to Receive.\r\n");
 
     for(pkt = 0; pkt < NUM_PACKETS; pkt++)
     {
-        /* Clear Buffer with 0xCC (Dummy) */
-        memset(gSlaveRxBuffer, 0xCC, APP_MCSPI_MSGSIZE);
+        MCSPI_Transaction_init(&spiTransaction);
+        spiTransaction.channel = MCSPI_CHANNEL_NUM;
+        spiTransaction.count   = APP_MCSPI_MSGSIZE;
+        spiTransaction.txBuf   = (void *)gSlaveTxBuffer; // Don't care
+        spiTransaction.rxBuf   = (void *)gSlaveRxBuffer;
+        
+        if(MCSPI_transfer(gMcspiHandle[CONFIG_MCSPI0], &spiTransaction) == SystemP_SUCCESS) {
+            /* Store Data */
+            memcpy(gStorage[pkt], gSlaveRxBuffer, APP_MCSPI_MSGSIZE);
+            DebugP_log("[SLAVE] Stored Pkt %d\r\n", pkt);
+        }
+    }
+
+    DebugP_log("[SLAVE] Receive Complete. Switching to Send...\r\n");
+
+    /* --- PHASE 2: SEND ALL --- */
+    for(pkt = 0; pkt < NUM_PACKETS; pkt++)
+    {
+        /* Retrieve Data */
+        memcpy(gSlaveTxBuffer, gStorage[pkt], APP_MCSPI_MSGSIZE);
 
         MCSPI_Transaction_init(&spiTransaction);
         spiTransaction.channel = MCSPI_CHANNEL_NUM;
         spiTransaction.count   = APP_MCSPI_MSGSIZE;
         spiTransaction.txBuf   = (void *)gSlaveTxBuffer;
-        spiTransaction.rxBuf   = (void *)gSlaveRxBuffer;
+        spiTransaction.rxBuf   = (void *)gSlaveRxBuffer; // Don't care
         
         if(MCSPI_transfer(gMcspiHandle[CONFIG_MCSPI0], &spiTransaction) == SystemP_SUCCESS) {
-            /* Print first 4 bytes */
-            DebugP_log("[SLAVE] Pkt %d: %02X %02X %02X %02X\r\n", 
-                       pkt, gSlaveRxBuffer[0], gSlaveRxBuffer[1], gSlaveRxBuffer[2], gSlaveRxBuffer[3]);
-        } else {
-            DebugP_log("[SLAVE] Error.\r\n");
-            MCSPI_close(gMcspiHandle[CONFIG_MCSPI0]);
-            MCSPI_open(CONFIG_MCSPI0, NULL);
+            DebugP_log("[SLAVE] Sent Pkt %d\r\n", pkt);
         }
     }
     
